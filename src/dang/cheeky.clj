405b633c5b414a749821c99dfd3c171209b57cfd
Nick Lawler <lawler145@gmail.com>
2020-09-12 16:02:42 -0400

    Get cheeky working
    Letfun thing is weird, I think just making a named fn and recursing with
    its name would work as well

--- src/dang/cheeky.clj
+++ src/dang/cheeky.clj
@@ -17,13 +17,27 @@
     [:app ?fn ?arg]
     `(~(compyl ?fn) ~(compyl ?arg))
 
-    [:fix [:lam ?var _ ?body]]
+    [:fix [:lam ?var _ (m/and [:lam ?var2 _ _] ?body)]]
+    ;; (walk/postwalk-replace {?var 'recur} (compyl ?body))
     ;; TODO still crashes on last example
-    ;; "can recur from tail position"
+    ;; "can only recur from tail position"
     ;; knowing what I know now, can prob replace
     ;; recur with a reference to the code of the 
     ;; recursive function
-    (walk/postwalk-replace {?var 'recur} (compyl ?body))
+
+    ;; but that doesn't work using let. crashes trying to resolve the 
+    ;; reference to the fun in the recursive definition itself
+    ;; would work with letfun, but how to get the arguments for non-function bodies?
+           ;; can only know if you pattern match into it
+
+    (let [label (gensym "fn-name")
+          compiled (walk/postwalk-replace {?var label} (compyl ?body))]
+      `(letfn [(~label [~?var2] ~(nth compiled 2))] ~compiled))
+    ;; then need a catchall for non inner fns
+    [:fix [:lam ?var _ ?body]]
+    (let [label (gensym "fn-name")
+          compiled (walk/postwalk-replace {?var label} (compyl ?body))]
+      `(let [~label ~compiled] ~compiled))
 
     [:let (m/symbol _ _ :as ?var) ?binding ?body]
     `(let ~[?var (compyl ?binding)] ~(compyl ?body))
@@ -64,9 +78,13 @@
   (evaluate [:fix [:lam 'x ::ignore 1]])
   ((fn named [] 1))
 
+  (letfn [(label [_x] 1)] 1)
+  (let [label (fn [x] 1)] 1)
+
   (loop [_x (fn [_x] 1)] 1)
 
   (loop [] 1)
+  (second '(fn [hi] hi))
 
   ((fn [gend]
      (loop [bleh gend]
@@ -77,6 +95,17 @@
      (println bleh)
      (if (dang.evaluate/is-zero bleh)
        0 (recur (dang.evaluate/pred bleh)))) 2)
+
+  (->> [:fix [:lam 'x ::ignore [:app :dang.ast/succ 1]]]
+       compyl
+       eval
+    ;;    second
+    ;;    (#(nth % 3))
+    ;;    compyl
+    ;;    (walk/postwalk-replace {'x 'my-name})
+    ;;    ((fn [arg] `(let ['myname ~arg] ~arg)))
+    ;;    eval
+       )
 
   (->> [:fix [:lam 'x ::ignore [:app :dang.ast/succ 1]]]
        second
@@ -98,6 +127,11 @@
     ;;    ((fn [arg] `(~arg 2)))
     ;;    eval
        )
+  (->> dang.evaluate/fix-realistic
+       compyl
+       eval)
+
   (is (= 0 (->> dang.evaluate/fix-realistic
                 compyl
-                eval))))
+                eval)))
+  (type (fn [hi] hi)))
