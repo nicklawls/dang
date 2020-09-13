9d1dfda460add48feac5cf26bbdb26ceffe2a9d7
Nick Lawler <lawler145@gmail.com>
2020-09-13 16:44:46 -0400

    Ponder features, add let, fix error in program

--- src/dang/db.clj
+++ src/dang/db.clj
@@ -28,8 +28,8 @@
         :if/then {:prim/nat 0}
         :if/else {:app/fn {:var/ref 'rec}
                   :app/arg {:app/fn :dang.ast/pred
-                            :app/arg {:var/ref 'bleh}}}}}}}}
-   :app/arg {:prim/nat 2}})
+                            :app/arg {:var/ref 'bleh}}}}}}}
+    :app/arg {:prim/nat 2}}})
 
 (def cfg {:store {:backend :mem :id "my-db"}})
 
@@ -62,6 +62,10 @@
          [:lam/type :db.type/ref :db.cardinality/one]
          [:lam/body :db.type/ref]
 
+         [:let/var :db.type/symbol]
+         [:let/binding :db.type/ref]
+         [:let/body :db.type/ref]
+
          [:if/cond :db.type/ref]
          [:if/then :db.type/ref]
          [:if/else :db.type/ref]
@@ -89,22 +93,105 @@
        :in $ ?sym
        :where
        [?lambda :lam/var ?sym]]
-     after-program
+     @conn
      'bleh)
 
 (->> (d/q '[:find (pull ?ref [:app/_fn])
             :in $ ?sym
             :where
             [?ref :var/ref ?sym]]
-          after-program
+          @conn
           'rec)
      first
      first
      :app/_fn)
 
 (d/q '[:find (pull ?prog [*])
-       :where [?prog :dang/program _]] after-program)
+       :where [?prog :dang/program _]] @conn)
+;; => ([{:db/id 21, :app/arg #:db{:id 36}, :dang/program #:db{:id 22}}])
 
+;; oh snap, accidentally had :app/arg on the program, not its application
+;; child. retract it from the program entity
+;; then add it to the program's :dang/program (baddly named attr)
+
+(def bad-ent (d/entity @conn 21))
+
+(def arg-id (:db/id (:app/arg bad-ent)))
+
+(d/transact
+ conn
+ [[:db/retract 21 :app/arg arg-id]
+  [:db/add (:db/id (:dang/program bad-ent)) :app/arg arg-id]])
+
+;; try again
+;; 
+(d/q '[:find (pull ?prog [*])
+       :where [?prog :dang/program _]] @conn)
+;; => ([{:db/id 21, :dang/program #:db{:id 22}}])
+
+(d/q '[:find (pull ?node [*])
+       :where [_ :dang/program ?node]] @conn)
+;; => ([{:db/id 22, :app/arg #:db{:id 36}, :app/fn #:db{:id 23}}])
+
+
+;; TODO: types of queries (and indices) depend upon the language, and
+;; the analyses we want to do
+;; 
+;; A language/environment for live-programmable web apps and dashboards
+;; * register data soruces from various web services and APIs
+;; * Markdown on crack
+;; * make disgustingly dense and interesting links on the fly and
+;; * send them to friends and family.
+;; * language for philosophy and love.
+;; 
+;; Analyses 
+;; * compiler optimizations (Martin odersky's compiler as database)
+;; * AOT virtual dom stuff (a'la svelte, whatever that newer thing is)
+;; * editor features
+;; * dataflow analysis
+;; * incremental computation
+;; * compile-to-cloud
+;; * code/data generation (from types/schemas)
+;; 
+;; Language Features
+;; * programs expressed as time-varying functions of the EAVT
+;;   database that is simultaneously
+;;   the codebase, database, and deployment registry,
+;;   and where execution traces can optionally be stored
+;;   * some of those functions are interpreted with datomic
+;;     transaction fn semantics
+;; * algebraic subtyping?
+;; * dependent types?
+;; * liquid/quotient types?
+;; * metaprogramming?
+;;   * even the haskellers can't stop talking about staging
+;; * algebraic effects?
+;; * based on term-rewriting calculi?
+;;   * not sure what practical benefits
+;;   * may be a way to unify dsl metaprogramming and algebraic effects?
+;; * hot reloading
+;;   * live editing and deployment
+;;   * diff and patch against a constantly running system
+;; * typed holes
+;; * text buffer saving as interface to semantic edit actions
+;; * simple semantic version control
+;;   * unison-like hashing or mechanized Rich Hickey rules
+;; * small core, most features in userland
+;; 
+;; example:
+;; color the let keyword dark blue, whole expresssion blue background
+;; color app red w/red background
+;; format: let
+'(search
+  [:let ?name ?binding ?body]
+  (row {:bg blue}
+       [(el {:color darkBlue} "let")
+        ()])) ;; => 
+
+;; Will probably be defined as instaparse transformer
+;; want the full syntax tree with spans and line/column info
+;; current parser forgets certain nodes and loses line info 
+;; on numbers, booleans, and symbols
 (defn ast->tx [_expr])
 
 (test/is (= program (ast->tx fix-realistic)))
